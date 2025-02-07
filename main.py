e8e6d92e78a1cb28aecdaa2fa390103071df57c2
srbh001 <22B3905@iitb.ac.in>
2025-02-08 01:25:46 +0530

    Initial Setup with fetch and post APIs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from influxdb_client import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+import datetime
 import os
 
 from base.routes import base_router
@@ -11,7 +12,6 @@ from base.routes import base_router
 load_dotenv()
 
 
-# Define settings using Pydantic's BaseSettings
 class Settings(BaseSettings):
     influxdb_url: str = os.getenv("INFLUXDB_URL")
     influxdb_token: str = os.getenv("INFLUXDB_TOKEN")
@@ -21,10 +21,15 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+BUCKET = settings.influxdb_bucket
+ORG = settings.influxdb_org
+
 
 influxdb_client = InfluxDBClient(
     url=settings.influxdb_url, token=settings.influxdb_token, org=settings.influxdb_org
 )
+write_api = influxdb_client.write_api()
+query_api = influxdb_client.query_api()
 
 # Security scheme for Bearer token
 security = HTTPBearer()
@@ -46,16 +51,49 @@ def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
 app = FastAPI()
 
 
+@app.post("/write_data/")
+async def write_data(I: float, V: float, P: float, device_id: str):
+
+    point = (
+        Point("sensor_data")
+        .tag("device_id", device_id)
+        .field("I", I)
+        .field("V", V)
+        .field("P", P)
+        .time(datetime.datetime.utcnow())
+    )  # Automatically adds timestamp
+
+    write_api.write(bucket=BUCKET, org=ORG, record=point)
+    return {"message": "Data written successfully"}
+
+
 @app.get("/query-data/")
 async def query_data():
-    query_api = influxdb_client.query_api()
     query = f"""
-    from(bucket: "{settings.influxdb_bucket}")
-      |> range(start: -1h)
-      |> filter(fn: (r) => r._measurement == "my_measurement")
+    from(bucket: "{BUCKET}")
+      |> range(start: -24h)
+      |> filter(fn: (r) => r._measurement == "sensor_data")
     """
-    result = query_api.query(query)
-    return {"data": result.to_json()}
+    tables = query_api.query(query, org=ORG)
+
+    formatted_results = {}
+
+    for table in tables:
+        for record in table.records:
+            device_id = record.values.get("device_id", "unknown")
+            timestamp = record.get_time().isoformat()
+            field = record.get_field()
+            value = record.get_value()
+
+            if device_id not in formatted_results:
+                formatted_results[device_id] = {}
+
+            if timestamp not in formatted_results[device_id]:
+                formatted_results[device_id][timestamp] = {}
+
+            formatted_results[device_id][timestamp][field] = value
+
+    return {"data": formatted_results}
 
 
 app.include_router(base_router, prefix="/base")
